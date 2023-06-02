9fb99f86c7022b275a2d01fac3ef64e8afda2be8
Bruno Damasceno Martins <bruno.damasceno@mercadolivre.com>
2023-06-02 15:17:04 -0300

    Adding minimal usage for toxiproxy

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/brunodmartins/toxiproxy-poc
+
+go 1.20
+
+require (
+	github.com/Shopify/toxiproxy v2.1.4+incompatible // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/sirupsen/logrus v1.9.2 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,56 @@
-package toxiproxy_poc
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	toxiproxy "github.com/Shopify/toxiproxy/client"
+	"net/http"
+)
+
+type ViaCepResponse struct {
+	Cep         string
+	Logradouro  string
+	Complemento string
+	Bairro      string
+	Localidade  string
+	UF          string
+}
+
+func (*ViaCepResponse) String() {
+
+}
+
+func SearchZipCode(host string, code string) (*ViaCepResponse, error) {
+	client := http.DefaultClient
+	response, err := client.Get(fmt.Sprintf("http://%s/ws/%s/json", host, code))
+	if err != nil {
+		return nil, err
+	}
+	result := new(ViaCepResponse)
+	buf := new(bytes.Buffer)
+	if _, err = buf.ReadFrom(response.Body); err != nil {
+		return nil, err
+	}
+	if err = json.Unmarshal(buf.Bytes(), result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func main() {
+	tx_client := toxiproxy.NewClient("localhost:8474")
+	proxy, err := tx_client.CreateProxy("viacep", "0.0.0.0:1234", "viacep.com.br:80")
+	defer proxy.Delete()
+	proxy.AddToxic("", "latency", "", 1, toxiproxy.Attributes{
+		"latency": 1000,
+	})
+	if err != nil {
+		panic(err)
+	}
+	response, err := SearchZipCode(proxy.Listen, "01001000")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v", response)
+}
